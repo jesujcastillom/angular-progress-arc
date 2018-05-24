4325142c31449e3820940be5bddafa16af858deb
Jesu Castillo <castillojesuj@gmail.com>
2018-05-24 16:09:11 -0400

    Change build implementation

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,43 +1,43 @@
-import path from "path";
-import UglifyJsPlugin from "uglifyjs-webpack-plugin";
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-const libraryName = "ng-progress-arc";
+const libraryName = 'ng-progress-arc';
 const outputFile = `${libraryName}.js`;
 
-export default {
-	entry: path.resolve(__dirname, "./angular-progress-arc.js"),
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: outputFile,
-		library: libraryName,
-		libraryTarget: "umd"
-	},
-	externals: {
-		angular: {
-			commonjs: "angular",
-			commonjs2: "angular",
-			root: "angular"
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel-loader"
-			},
-			{
-				test: /\.html$/,
-				use: [{
-					loader: 'html-loader',
-					options: {
-						minimize: true
-					}
-				}]
-			}
-		]
-	},
-	plugins: [
-		new UglifyJsPlugin()
-	]
-}
+module.exports = {
+  entry: path.resolve(__dirname, './angular-progress-arc.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+  },
+  externals: {
+    angular: {
+      commonjs: 'angular',
+      commonjs2: 'angular',
+      root: 'angular',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            minimize: true,
+          },
+        }],
+      },
+    ],
+  },
+  plugins: [
+    new UglifyJsPlugin(),
+  ],
+};
